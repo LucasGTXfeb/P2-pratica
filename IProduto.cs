330cadf06d1995e403169c25e7d10dc2f281bca4
LucasGTXfeb <lucas.gcsilva@sou.unifeb.edu.br>
2025-06-07 18:44:24 -0300

    Criando entidade Produto com validações e interface de contrato simples

---
+++ IProduto.cs
@@ -0,0 +1,10 @@
+namespace LojaVirtual.Entities
+{
+    public interface IProduto
+    {
+        int Id { get; }
+        string Nome { get; }
+        decimal Preco { get; }
+        string Categoria { get; }
+    }
+}
--- Produto.cs
+++ Produto.cs
@@ -1,1 +1,28 @@
+using System;
+
+namespace LojaVirtual.Entities
+{
+    public class Produto : IProduto
+    {
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public decimal Preco { get; private set; }
+        public string Categoria { get; private set; }
+
+        public Produto(int id, string nome, decimal preco, string categoria)
+        {
+            if (string.IsNullOrWhiteSpace(nome))
+                throw new ArgumentException("Nome do produto inválido.");
+            if (preco <= 0)
+                throw new ArgumentException("Preço deve ser maior que zero.");
+            if (string.IsNullOrWhiteSpace(categoria))
+                throw new ArgumentException("Categoria inválida.");
+
+            Id = id;
+            Nome = nome;
+            Preco = preco;
+            Categoria = categoria;
+        }
+    }
+}
 
